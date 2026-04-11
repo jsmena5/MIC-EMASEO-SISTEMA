0369de510196ec125cae58ec82fa43ba3cb25311
James Mena <jsmena5@espe.edu.ec>
2026-04-11 14:43:19 -0500

    Nuevos Cambios

--- Backend/api-gateway/src/index.js
+++ Backend/api-gateway/src/index.js
@@ -3,33 +3,30 @@ import cors from "cors"
 import morgan from "morgan"
 import { createProxyMiddleware } from "http-proxy-middleware"
 
-import { verifyToken } from "./middlewares/auth.middleware.js"
-
 const app = express()
 
 app.use(cors())
-app.use(express.json())
 app.use(morgan("dev"))
 
-/**
- *  PUBLICAS
- */
+// ✅ AUTH (CORRECTO)
 app.use("/api/auth", createProxyMiddleware({
   target: "http://localhost:3002",
   changeOrigin: true,
-  pathRewrite: {
-    "^/api": ""
+  logLevel: "debug",
+  pathRewrite: (path, req) => {
+    return "/api/auth" + path   // 🔥 CLAVE ABSOLUTA
   }
 }))
 
+// ✅ USERS (CORRECTO)
 app.use("/api/users", createProxyMiddleware({
   target: "http://localhost:3000",
   changeOrigin: true,
-  pathRewrite: {
-    "^/api": ""
+  logLevel: "debug",
+  pathRewrite: (path, req) => {
+    return "/api/users" + path
   }
 }))
-
 
 app.listen(4000, () => {
   console.log("API Gateway running on port 4000")
